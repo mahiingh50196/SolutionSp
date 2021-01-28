cc9e1133464b2e1ba6e277e9bfde5480f6e2e1cf
Shubham <deol.shubham@gmail.com>
2021-01-28 14:55:17 +0530

    feat: more fixes

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import CountryPicker from "./CountryPicker";
 import SocialLogin from "./SocialLogin";
-import Map from "./Map";
 import CarServices from "./CarServices";
 import ImagePick from "./ImagePick";
 
-export { CountryPicker, SocialLogin, Map, CarServices, ImagePick };
+export { CountryPicker, SocialLogin, CarServices, ImagePick };
